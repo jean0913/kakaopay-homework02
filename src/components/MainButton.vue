1e818321458ed336676caa8d9f2e5562cff102f8
Jean <soyou913@gmail.com>
2020-01-20 00:33:30 +0900

    component edit

--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -9,6 +9,6 @@ export default{
 }
 </script>
 <style>
-.btn-wrap{width:100%;background-color:#ffdf00;margin-top:50px;border:1px solid #fcd70d;border-radius:5px;text-align:center;}
+.btn-wrap{display:block;width:100%;background-color:#ffdf00;margin-top:50px;border:1px solid #fcd70d;border-radius:5px;text-align:center;cursor:pointer;}
 .btn-wrap button{display:inline-block;height:48px;padding:0px 48px;background-color:#ffdf00;color:#1b1b1b;line-height:48px;font-size:18px;font-weight:bold;text-align:center;}
 </style>
